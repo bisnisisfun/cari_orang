d1380bd26e8773dfd5c144fe11f4de6156d7e2f1
Bisnisisfun <bisnisisfun@gmail.com>
2023-05-27 14:56:29 +0700

    ganti script

--- track_phone.py
+++ track_phone.py
@@ -1,7 +1,9 @@
 import requests
 
 def track_phone(imei):
-    url = f"https://us1.unwiredlabs.com/v2/process.php?token=pk.93ec4f4ae65a325b7d785c02eec583ff&imei={imei}"
+    api_token = "pk.93ec4f4ae65a325b7d785c02eec583ff"  # Ganti dengan token API yang Anda dapatkan setelah mendaftar di OpenCelliD
+    url = f"https://us1.unwiredlabs.com/v2/process.php?token={api_token}&imei={imei}"
+    
     response = requests.get(url)
     data = response.json()
     
@@ -12,8 +14,6 @@ def track_phone(imei):
         print(f"Latitude: {latitude}, Longitude: {longitude}, Accuracy: {accuracy} meters")
     else:
         print("Gagal melacak telepon")
-
-# Ganti 'YOUR_API_TOKEN' dengan token API yang Anda dapatkan setelah mendaftar di OpenCelliD
 
 imei_number = "86913003490637"  # Ganti dengan nomor IMEI perangkat yang ingin Anda lacak
 track_phone(imei_number)
